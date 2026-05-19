6e4970ad13f6f9301d1ae52c6a714eb14ce4241a
adrianhonrubia <adrian.honrubia@estudiantat.upc.edu>
2026-05-19 23:41:43 +0200

    Tasca 7

--- AgroVerse/Assets/Scripts/TaskUI.cs
+++ AgroVerse/Assets/Scripts/TaskUI.cs
@@ -1,24 +1,13 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 public class TaskUI : MonoBehaviour
 {
     [Header("Referencias")]
     public TextMeshProUGUI taskListText;
-    public Transform playerCamera; // La cámara del XR Origin
-
-    [Header("Posición flotante")]
-    public Vector3 offsetFromCamera = new Vector3(0f, -0.3f, 1.2f);
-    public float followSpeed = 3f;
 
     void Start()
     {
-        // Si no hay referencia manual, buscar la cámara
-        if (playerCamera == null)
-            playerCamera = Camera.main?.transform;
-
-        // Suscribirse al TaskManager
         if (TaskManager.Instance != null)
         {
             TaskManager.Instance.onTaskCompleted.AddListener(OnTaskCompleted);
@@ -26,18 +15,6 @@ public class TaskUI : MonoBehaviour
         }
 
         UpdateUI();
-    }
-
-    void LateUpdate()
-    {
-        // La UI sigue a la cámara suavemente
-        if (playerCamera != null)
-        {
-            Vector3 targetPos = playerCamera.TransformPoint(offsetFromCamera);
-            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * followSpeed);
-            transform.LookAt(playerCamera);
-            transform.Rotate(0, 180, 0); // Girar para que mire al jugador
-        }
     }
 
     void OnTaskCompleted(string taskId)
