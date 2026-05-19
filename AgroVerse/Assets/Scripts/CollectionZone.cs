68357358f973d85512d0129c01a1778cf088540e
adrianhonrubia <adrian.honrubia@estudiantat.upc.edu>
2026-05-20 00:06:40 +0200

    Fusión mecanicas entrega 1

--- AgroVerse/Assets/Scripts/CollectionZone.cs
+++ AgroVerse/Assets/Scripts/CollectionZone.cs
@@ -2,19 +2,14 @@ using UnityEngine;
 
 public class CollectionZone : MonoBehaviour
 {
-    [Header("Qué acepta esta zona")]
-    public string acceptedTag = "Egg";
-
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(acceptedTag))
+        EggPickup egg = other.transform.root.GetComponentInChildren<EggPickup>();
+
+        if (egg != null && !egg.hasBeenCollected)
         {
-            EggPickup egg = other.GetComponent<EggPickup>();
-            if (egg != null && egg.IsHeld == false)
-            {
-                // El huevo ha sido soltado dentro de la zona
-                egg.Collect();
-            }
+            egg.Collect();
+            Debug.Log("[CollectionZone] Huevo recogido.");
         }
     }
 }
--- AgroVerse/Assets/Scripts/EggPickup.cs
+++ AgroVerse/Assets/Scripts/EggPickup.cs
@@ -1,30 +1,18 @@
 using UnityEngine;
 
-public class EggPickup : PickupObject
+public class EggPickup : MonoBehaviour
 {
-    [Header("Huevo")]
     public bool hasBeenCollected = false;
 
-    // Se llama cuando el huevo entra en la zona de colección
     public void Collect()
     {
         if (hasBeenCollected) return;
 
         hasBeenCollected = true;
-        canPickup = false;
-
         Debug.Log("[EggPickup] Huevo recogido correctamente.");
 
-        // Avisar al TaskManager
         TaskManager.Instance?.CompleteTask("recoger_huevo");
 
-        // Destruir o desactivar el huevo
-        gameObject.SetActive(false);
-    }
-
-    public override void  OnInteract(InteractionType type)
-    {
-        base.OnInteract(type);
-        Debug.Log($"[EggPickup] Huevo interactuado con: {type}");
+        transform.root.gameObject.SetActive(false);
     }
 }
